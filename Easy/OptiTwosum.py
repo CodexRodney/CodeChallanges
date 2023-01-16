2db8e1d16a0ff1fbe8cbd1dec58a4d4c3496dccf
CodexRodney <rodney002mwanje@gmail.com>
2023-01-16 14:19:06 +0300

    Added Optimized version of the Twosum{

---
+++ Easy/OptiTwosum.py
@@ -0,0 +1,11 @@
+"""
+Optimized Solution by arajAnkit
+Time Complexity: O(N)
+"""
+ def twoSum(nums, target):
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            numToIndex[nums[i]] = i
+        return []
